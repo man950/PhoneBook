658b724e0962d2d769c29be458c5b6521c06cfd4
Maneh <simman950@gmail.com>
2024-04-12 21:35:58 +0400

    info changed

--- phonebook/main.py
+++ phonebook/main.py
@@ -1,10 +1,48 @@
-from functions import (
+'''from functions import (
     add_contact,
     delete_contact,
     find_contact,
     show_all_contacts,
     ask_contact,
-)
+)'''
+
+
+
+def ask_contact(contacts):
+    name = input("Введите имя: ")
+    middle_name= input("Введите отчество: ")
+    second_name = input("Введите фамилию: ")
+    number = input("Введите номер телефона: ")
+    contacts = {'Имя': name, 'Отсечтво': middle_name, 'Фамилия': second_name, 'Номер телефона': number }
+    print(f"Контакт {name} добавлен.")
+  
+def add_contact(phone_book):
+    ask_contact(phone_book, contacts,  'a')
+    return contacts
+
+
+
+def delete_contact(phone_book, name):
+    if name in phone_book:
+        del phone_book[name]
+        print(f"Контакт {name} удален.")
+    else:
+        print(f"Контакт {name} не найденю")
+
+
+def find_contact(phone_book, name):
+    if name in phone_book:
+        print(f"{name}: {phone_book[name]}")
+    else:
+        print(f"Контакт {name} не найденю")
+
+
+def show_all_contacts(phone_book, name, number):
+    if phone_book:
+        for name, number in phone_book.items():
+            print(f"{name}: {number}")
+        else:
+            print("Телефонная книга пуста:")
 
 
 def main_menu():
@@ -18,7 +56,7 @@ def main_menu():
         choice = input("Выберите действие: ")
 
         if choice == "1":
-            add_contact(phone_book, name, middle_name, second_name, number)
+            ask_contact(phone_book)
         elif choice == "2":
             name = input("Введите имя для удаления: ")
             delete_contact(phone_book, name)
